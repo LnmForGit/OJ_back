428a34b2d4f2226dc699ddd321ad466f9991dc1f
PANQIHANG <954347921@qq.com>
2019-04-18 13:27:09 +0800

    首页

---
+++ src/main/java/com/oj/schedule/demo.java
@@ -0,0 +1,62 @@
+package com.oj.schedule;
+
+import com.oj.mapper.system.HomepageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Created by AC on 2019/4/17 14:46
+ */
+@Component
+public class demo {
+
+    public static String stampToDate(String s){
+        String res;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd");
+        long lt = new Long(s)*1000;
+        Date date = new Date(lt);
+        res = simpleDateFormat.format(date);
+        return res;
+    }
+    public static long dateToStamp(String s) throws ParseException
+    {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = simpleDateFormat.parse(s);
+        long res = date.getTime()/1000;
+        return res;
+    }
+    //注入UserMapper
+    @Autowired(required = false)
+    private HomepageMapper homepagemapper;
+
+    @Scheduled(cron = "0 0 6 * * *")
+    public void firstScheduledTasks(){
+        Calendar cal=Calendar.getInstance();
+        int y=cal.get(Calendar.YEAR);
+        int m=cal.get(Calendar.MONTH)+1;
+        int d=cal.get(Calendar.DATE);
+        String datetime = y+"-"+m+"-"+d;
+        try
+        {
+            long Stime = dateToStamp(datetime);
+            homepagemapper.clearmonth();
+            for(int i=0;i<30;i++)
+            {
+                long start = Stime-i*86400;
+                long end = start + 86400;
+                String starttime=String.valueOf(start),endtime=String.valueOf(end);
+                String res = homepagemapper.getsubmit(starttime,endtime);
+                String resac = homepagemapper.getac(starttime,endtime);
+                starttime = stampToDate(starttime);
+                homepagemapper.savemonth(starttime,resac,res);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
